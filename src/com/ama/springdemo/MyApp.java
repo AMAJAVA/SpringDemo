1fd329727b8b9662c072e23d83d1d74cf35d9cd6
AMAJAVA <midosa7li@gmail.com>
2018-12-30 14:57:01 +0100

    AMA: modifier fichier .gitignore

--- src/com/ama/springdemo/MyApp.java
+++ src/com/ama/springdemo/MyApp.java
@@ -3,11 +3,10 @@ package com.ama.springdemo;
 public class MyApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 // create the object
-	    Coach theCoach = new TrackCoach();
+		// create the object
+	    	Coach theCoach = new TrackCoach();
 	    // use the object
-	    System.out.println(theCoach.getDailyWorkout());
+	    	System.out.println(theCoach.getDailyWorkout());
 	}
 
 }
