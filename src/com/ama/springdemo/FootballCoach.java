6f50d8d1271df759d92cbacfa6b96f62f19497cb
AMAJAVA <midosa7li@gmail.com>
2018-12-30 17:02:28 +0100

    AMA: Exercice application Spring Container

---
+++ src/com/ama/springdemo/FootballCoach.java
@@ -0,0 +1,10 @@
+package com.ama.springdemo;
+
+public class FootballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Essayer de marquer 3 coup francs direct!!!";
+	}
+
+}
--- src/com/ama/springdemo/HelloSpringApp.java
+++ src/com/ama/springdemo/HelloSpringApp.java
@@ -8,7 +8,7 @@ public class HelloSpringApp {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         
         // retrieve bean from spring container 
-        Coach theCoach = context.getBean("myCoach", Coach.class);
+        Coach theCoach = context.getBean("footballCoach", Coach.class);
         
         // call methods on the bean 
         System.out.println(theCoach.getDailyWorkout());
